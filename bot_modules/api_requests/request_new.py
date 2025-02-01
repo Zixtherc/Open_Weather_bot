6a4ec76e3ce913c7819d123181b85754b46e1a8a
Zixther <duckandfiretto@gmail.com>
2025-02-02 00:35:36 +0200

    update to request_news func

--- bot_modules/api_requests/request_new.py
+++ bot_modules/api_requests/request_new.py
@@ -4,36 +4,48 @@ import json
 from ..jsn_function.read_json import read_json
 from ..jsn_function.load_json import load_json
 
-from os.path import abspath, join
-import json
 
-def load_json(name_json : str, value_file : str):
-    path = abspath(join(__file__, '..', '..', 'static', name_json))
-    
-    with open (file = path, mode = "w", encoding = "utf-8") as file:
-        json.dump(
-            obj = value_file,
-            ensure_ascii = False,
-            indent = 4,
-            fp = file
-        )
-api_dict = read_json(name_json = "config.json")
+
+api_dict = read_json(name_json = "config_api.json")
 
 API_KEY = api_dict["api_key_news"]
 
 
-def request_news(country: str = "ua"):
-    URL = f"https://newsapi.org/v2/top-headlines?sources=bbc-news,cnn&apiKey={API_KEY}"
+def request_news(country: str = "USA", count : int = 0, flag_country : bool = False):
 
-    response = requests.get(URL)
+    if flag_country:
+        print(f'Зашло ')
+        URL = f"https://newsapi.org/v2/top-headlines?q={country}&apiKey={API_KEY}"
 
-    if response.status_code == 200:
-        news_data = json.loads(response.content)
-        load_json(name_json="load_news.json", value_file=news_data)
-        current = news_data["articles"][0]["source"]["id"]
-        print(current)
+        response = requests.get(URL)
+        if response.status_code == 200:
+
+            news_data = json.loads(response.content)
+            load_json(name_json="load_news.json", value_file=news_data)
+            news_id = news_data["articles"][count]
+
+            news_author = news_id["author"]
+            news_title = news_id["title"]
+            news_description = news_id["description"]
+            news_source = news_id["url"]
+            news_time = news_id["publishedAt"]
+
+            return news_author, news_title, news_description, news_source, news_time
+
+        else:
+            print(f'Неудачная попытка реквеста')
     else:
-        print(f'Неудачная попытка реквеста, соси член')
+        URL = f"https://newsapi.org/v2/top-headlines?sources=bbc-news,cnn&apiKey={API_KEY}"
+
+        response = requests.get(URL)
+
+        if response.status_code == 200:
+            news_data = json.loads(response.content)
+            load_json(name_json="load_news.json", value_file=news_data)
+            current = news_data["articles"][count]["source"]["id"]
+            print(current)
+        else:
+            print(f'Неудачная попытка реквеста')
 
 
-request_news(country= "ua")
+request_news(count = 0, flag_country = True)
